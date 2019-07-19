34623adc8fcf8405a05052752f0e9d9f22435c5f
Lawthom1410 <jack.lawthom@academytrainee.com>
2019-07-19 11:49:54 +0100

    rest create tournament

---
+++ src/main/java/qa/com/JavaEE_Project_JackLawthom/rest/JAXActivator.java
@@ -0,0 +1,9 @@
+package qa.com.JavaEE_Project_JackLawthom.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JAXActivator extends Application {
+
+}
