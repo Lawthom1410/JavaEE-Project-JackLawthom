d705de5de9ea96fdb4cd5e4ec55de21d80f8eb6f
Lawthom1410 <jack.lawthom@academytrainee.com>
2019-07-19 11:34:36 +0100

    service create tournament

--- src/main/java/qa/com/JavaEE_Project_JackLawthom/service/TournamentServiceImpl.java
+++ src/main/java/qa/com/JavaEE_Project_JackLawthom/service/TournamentServiceImpl.java
@@ -1,5 +1,17 @@
 package qa.com.JavaEE_Project_JackLawthom.service;
 
+import javax.inject.Inject;
+
+import qa.com.JavaEE_Project_JackLawthom.persistence.repo.TournamentDBRepo;
+
 public class TournamentServiceImpl {
+	
+	@Inject
+	private TournamentDBRepo repo;
+	
+	public String createTournament(String tournament) {
+		return this.repo.createTournament(tournament);
+	}
+	
 
 }
