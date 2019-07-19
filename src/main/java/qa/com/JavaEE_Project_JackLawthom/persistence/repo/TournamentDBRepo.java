c725801f534d65b9fd5cddf8b3abaecacd59fc6e
Lawthom1410 <jack.lawthom@academytrainee.com>
2019-07-19 11:17:38 +0100

    persistence create tournament

--- src/main/java/qa/com/JavaEE_Project_JackLawthom/persistence/repo/TournamentDBRepo.java
+++ src/main/java/qa/com/JavaEE_Project_JackLawthom/persistence/repo/TournamentDBRepo.java
@@ -3,10 +3,13 @@ package qa.com.JavaEE_Project_JackLawthom.persistence.repo;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
 
 import qa.com.JavaEE_Project_JackLawthom.persistence.domain.Tournament;
 import qa.com.JavaEE_Project_JackLawthom.util.JSONUtil;
 
+@Transactional(value=TxType.SUPPORTS)
 public class TournamentDBRepo {
 	
 	@Inject
@@ -15,6 +18,7 @@ public class TournamentDBRepo {
 	@PersistenceContext(unitName = "primary")
 	private EntityManager em;
 	
+	@Transactional(value=TxType.REQUIRED)
 	public String createTournament(String tournament) {
 		this.em.persist(this.gson.convertJson(tournament, Tournament.class));
 		return "Creation Success";		
