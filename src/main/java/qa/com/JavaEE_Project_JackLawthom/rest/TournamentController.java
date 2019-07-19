4a1d2b5532500cd99b6948792bd48b500bf9782a
Lawthom1410 <jack.lawthom@academytrainee.com>
2019-07-19 12:47:14 +0100

    delete tournament

--- src/main/java/qa/com/JavaEE_Project_JackLawthom/rest/TournamentController.java
+++ src/main/java/qa/com/JavaEE_Project_JackLawthom/rest/TournamentController.java
@@ -1,6 +1,7 @@
 package qa.com.JavaEE_Project_JackLawthom.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
@@ -16,6 +17,12 @@ public class TournamentController {
 	@POST
 	public String createTournament(String tournament) {
 		return this.service.createTournament(tournament);
+	}
+	
+	@Path("/delete")
+	@DELETE
+	public String deleteTournament(long id) {
+		return this.service.deleteTournament(id);
 	}
 
 }
