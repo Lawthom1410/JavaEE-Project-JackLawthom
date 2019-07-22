5055c93f33dd3babe07801b40b165c9758899c84
Lawthom1410 <jack.lawthom@academytrainee.com>
2019-07-22 17:36:54 +0100

    add matches table to api

--- src/main/java/qa/com/JavaEE_Project_JackLawthom/persistence/repo/TournamentDBRepo.java
+++ src/main/java/qa/com/JavaEE_Project_JackLawthom/persistence/repo/TournamentDBRepo.java
@@ -1,5 +1,7 @@
 package qa.com.JavaEE_Project_JackLawthom.persistence.repo;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -9,6 +11,7 @@ import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 
 import qa.com.JavaEE_Project_JackLawthom.persistence.domain.Tournament;
+import qa.com.JavaEE_Project_JackLawthom.persistence.domain.Match;
 import qa.com.JavaEE_Project_JackLawthom.util.JSONUtil;
 
 @Transactional(value=TxType.SUPPORTS)
@@ -49,6 +52,37 @@ public class TournamentDBRepo {
 	
 	public String getAllTournaments() {
 		TypedQuery<Tournament> query = em.createQuery("SELECT t FROM Tournament t", Tournament.class);
+		return this.gson.getJson(query.getResultList());
+	}
+	
+	@Transactional(value=TxType.REQUIRED)
+	public String createMatch(String match ) {
+		this.em.persist(this.gson.convertJson(match, Match.class));
+		return "Creation Success";	
+	}
+	
+	@Transactional(value=TxType.REQUIRED)
+	public String deleteMatch(long id) {
+		this.em.remove(this.em.find(Match.class, id));
+		return "Deletion Success";
+	}
+
+	@Transactional(value=TxType.REQUIRED)
+	public String updateMatch(long id, String match) {
+		Match oldMatch = this.em.find(Match.class, id);
+		Match newMatch = gson.convertJson(match, Match.class);
+		
+		oldMatch.setNamePlayer1(newMatch.getNamePlayer1());
+		oldMatch.setNamePlayer2(newMatch.getNamePlayer2());
+		oldMatch.setTreeRow(newMatch.getTreeRow());
+		oldMatch.setTreeCol(newMatch.getTreeCol());
+		oldMatch.setTournamentId(newMatch.getTournamentId());
+		this.em.persist(oldMatch);
+		return "Update Success";
+	}
+	
+	public String getTournamentMatches(long id) {
+		TypedQuery<Match> query = em.createQuery("SELECT m FROM Match m WHERE m.tournamentId = '"+id+"'", Match.class);
 		return this.gson.getJson(query.getResultList());
 	}
 }
